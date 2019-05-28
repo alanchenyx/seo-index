063191cee2e8d30e9d14acdbdeba7b95cdabcf9a
alanchenyx <alanchenyixin@gmail.com>
2019-05-28 15:07:50 +1000

    refactoring code

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import csv
+import datetime
 import sitemap
 import indextagger
 def main():
-    print('welcome')
+    print('welcome, starting at '+str(datetime.datetime.now()))
 
     # get url list from xml sitemap
     # sitemaplink = raw_input('Enter your sitemap link to start~ \n')
@@ -14,10 +16,27 @@ def main():
     # urlList = ['https://thethreeseas.com.au/schoolies/', 'https://www.gawertfwefawefasdfaedasfaswfe.com/',
     #            'https://thethreeseas.com.au/universalchildrensday/', 'https://thethreeseas.com.au/news-test/']
 
-    for url in urlList:
-        indextagger.tag_url(url)
 
+    with open('result.csv', mode='w') as resultOutput:
+        writeToFile = csv.writer(resultOutput, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+        for url in urlList:
+            if indextagger.tag_url(url):
+                print(url + '   ' + 'indexed')
+                writeToFile.writerow([url, 'indexed'])
+                resultOutput.flush()
+            if not indextagger.tag_url(url):
+                print(url + '   ' + 'NOT indexed')
+                writeToFile.writerow([url, 'NOT indexed'])
+                resultOutput.flush()
+
+    print('all done, finished at ' +str(datetime.datetime.now()))
+
+    urlList = ['thethreeseas.com.au']
+
+    indextagger.tag_url('thethreeseas.com.au')
+
+    print('all done, finished at ' +str(datetime.datetime.now()))
 
 
 if __name__ == "__main__":
